72cb61a3c40182ee90ae20dba146b57c988c1468
CarefreeLife <csm4903@naver.com>
2024-02-19 00:25:14 +0900

    [20270219] 스프링 기초

--- 스프링 핵심원리 기본편 2회차 학습/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심원리 기본편 2회차 학습/core/src/main/java/hello/core/AppConfig.java
@@ -1,12 +1,18 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+
+/**
+ * AppConfig 라는 설정 클래스를 통해 각 역할의 구현체를 한번에 지정할 수 있다.
+ * - 사용 영역의 코드는 손대지 않고, 구성 영역의 코드만 변경하여 OCP 원칙을 잘 지킬수 있게 되었다.
+ */
 
 public class AppConfig {
 
@@ -23,6 +29,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
     }
 }
